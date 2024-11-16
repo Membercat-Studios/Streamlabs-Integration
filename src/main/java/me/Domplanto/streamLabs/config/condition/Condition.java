4b185d6844685217bc7aa513c21e52d9cb499ffc
codingcat2468 <lw-app-produktion@gmx.net>
2024-11-16 19:54:59 +0100

    Make conditions invertible by adding "!"

--- src/main/java/me/Domplanto/streamLabs/config/condition/Condition.java
+++ src/main/java/me/Domplanto/streamLabs/config/condition/Condition.java
@@ -14,33 +14,40 @@ public class Condition {
     private final Function<JsonObject, String> element1;
     private final Function<JsonObject, String> element2;
     private final Operator operator;
+    private final boolean invert;
 
-    private Condition(Operator operator, Function<JsonObject, String> element1, Function<JsonObject, String> element2) {
+    private Condition(Operator operator, boolean invert, Function<JsonObject, String> element1, Function<JsonObject, String> element2) {
         this.element1 = element1;
         this.element2 = element2;
         this.operator = operator;
+        this.invert = invert;
     }
 
     public boolean check(JsonObject object) {
         String e1 = element1.apply(object);
         String e2 = element2.apply(object);
         try {
-            return this.operator.check(Double.parseDouble(e1), Double.parseDouble(e2));
+            return invert != this.operator.check(Double.parseDouble(e1), Double.parseDouble(e2));
         } catch (NumberFormatException e) {
-            return this.operator.check(e1, e2);
+            return invert != this.operator.check(e1, e2);
         }
     }
 
     public static List<Condition> parseAll(List<String> conditionStrings, StreamlabsEvent event) {
         return conditionStrings.stream()
                 .map(string -> {
+                    boolean invert = string.startsWith("!");
+                    if (invert)
+                        string = string.substring(1);
+
+                    final String finalString = string;
                     Operator op = OPERATORS.stream()
-                            .filter(operator1 -> string.contains(operator1.getName()))
+                            .filter(operator1 -> finalString.contains(operator1.getName()))
                             .findFirst().orElse(null);
                     if (op == null) return null;
 
-                    String[] elements = string.split(op.getName());
-                    return new Condition(op, parseElement(elements[0], event), parseElement(elements[1], event));
+                    String[] elements = finalString.split(op.getName());
+                    return new Condition(op, invert, parseElement(elements[0], event), parseElement(elements[1], event));
                 }).toList();
     }
 
