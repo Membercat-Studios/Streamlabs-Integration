9b0dd649c018db628b57f561b9ddbbb973e0b0d4
codingcat2468 <lw-app-produktion@gmx.net>
2024-11-27 01:14:59 +0100

    Add centered message type (because maz wanted to)

--- src/main/java/me/Domplanto/streamLabs/message/MessageType.java
+++ src/main/java/me/Domplanto/streamLabs/message/MessageType.java
@@ -1,11 +1,13 @@
 package me.Domplanto.streamLabs.message;
 
+import me.Domplanto.streamLabs.font.DefaultFontInfo;
 import org.bukkit.entity.Player;
 
 import java.util.function.BiConsumer;
 
 public enum MessageType {
     MESSAGE(Player::sendMessage),
+    MESSAGE_CENTERED((player, message) -> player.sendMessage(DefaultFontInfo.centerMessage(message))),
     @SuppressWarnings("deprecation")
     TITLE((player, message) -> player.sendTitle(message, null)),
     @SuppressWarnings("deprecation")
