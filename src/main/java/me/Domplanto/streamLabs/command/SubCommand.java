4337c808ed6d3637f0e4a07f6ae367bcc498441e
codingcat2468 <lw-app-produktion@gmx.net>
2025-02-14 07:40:55 +0100

    Switch to brigadier command api

--- src/main/java/me/Domplanto/streamLabs/command/SubCommand.java
+++ src/main/java/me/Domplanto/streamLabs/command/SubCommand.java
@@ -1,20 +1,38 @@
 package me.Domplanto.streamLabs.command;
 
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
 import me.Domplanto.streamLabs.StreamLabs;
 import me.Domplanto.streamLabs.util.ReflectUtil;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.TabCompleter;
+import me.Domplanto.streamLabs.util.components.Translations;
+import org.bukkit.command.CommandSender;
 
 import java.util.Set;
+import java.util.function.Consumer;
+import java.util.logging.Level;
 
-public abstract class SubCommand implements TabCompleter, CommandExecutor {
+@SuppressWarnings("UnstableApiUsage")
+public abstract class SubCommand {
     private final StreamLabs pluginInstance;
 
     public SubCommand(StreamLabs pluginInstance) {
         this.pluginInstance = pluginInstance;
     }
 
-    public abstract String getName();
+    public abstract LiteralCommandNode<CommandSourceStack> buildCommand();
+
+    public final int exceptionHandler(CommandContext<CommandSourceStack> source, Consumer<CommandSender> action) {
+        try {
+            action.accept(source.getSource().getSender());
+        } catch (Exception e) {
+            pluginInstance.getLogger().log(Level.SEVERE, "Unexpected error while trying to execute command", e);
+            source.getSource().getSender().sendMessage(Translations.withPrefix(Translations.UNEXPECTED_ERROR, true));
+        }
+
+        return Command.SINGLE_SUCCESS;
+    }
 
     protected StreamLabs getPlugin() {
         return this.pluginInstance;
