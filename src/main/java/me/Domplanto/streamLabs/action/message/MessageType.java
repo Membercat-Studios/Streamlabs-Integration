24ab51f1ecdffbcc035929f50d68fd2db6556821
codingcat2468 <lw-app-produktion@gmx.net>
2025-02-15 23:13:11 +0100

    Fix title messages not sending correctly

--- src/main/java/me/Domplanto/streamLabs/action/message/MessageType.java
+++ src/main/java/me/Domplanto/streamLabs/action/message/MessageType.java
@@ -2,7 +2,7 @@ package me.Domplanto.streamLabs.action.message;
 
 import me.Domplanto.streamLabs.util.font.DefaultFontInfo;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.title.Title;
+import net.kyori.adventure.title.TitlePart;
 import org.bukkit.entity.Player;
 
 import java.util.function.BiConsumer;
@@ -10,8 +10,8 @@ import java.util.function.BiConsumer;
 public enum MessageType {
     MESSAGE(Player::sendMessage),
     MESSAGE_CENTERED((player, message) -> player.sendMessage(DefaultFontInfo.centerMessage(message))),
-    TITLE((player, message) -> player.showTitle(Title.title(message, Component.empty()))),
-    SUBTITLE((player, message) -> player.showTitle(Title.title(Component.empty(), message)));
+    TITLE((player, message) -> player.sendTitlePart(TitlePart.TITLE, message)),
+    SUBTITLE((player, message) -> player.sendTitlePart(TitlePart.SUBTITLE, message));
 
     private final BiConsumer<Player, Component> action;
 
