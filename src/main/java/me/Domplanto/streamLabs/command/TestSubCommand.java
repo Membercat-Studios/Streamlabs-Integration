3945086d0d00d239c50a279d51de6127489f6dfb
codingcat2468 <lw-app-produktion@gmx.net>
2025-02-15 22:18:24 +0100

    Re-implement test & reload commands

--- src/main/java/me/Domplanto/streamLabs/command/TestSubCommand.java
+++ src/main/java/me/Domplanto/streamLabs/command/TestSubCommand.java
@@ -1,106 +1,130 @@
 package me.Domplanto.streamLabs.command;
 
 import com.google.gson.JsonObject;
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.context.ParsedCommandNode;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import com.mojang.brigadier.tree.CommandNode;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.argument.CustomArgumentType;
 import me.Domplanto.streamLabs.StreamLabs;
+import me.Domplanto.streamLabs.command.argument.EventArgumentType;
 import me.Domplanto.streamLabs.config.ActionPlaceholder;
 import me.Domplanto.streamLabs.events.StreamlabsEvent;
 import me.Domplanto.streamLabs.events.youtube.YoutubeSuperchatEvent;
 import me.Domplanto.streamLabs.util.components.ColorScheme;
 import me.Domplanto.streamLabs.util.components.Translations;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
+import org.apache.commons.lang3.tuple.Pair;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Objects;
 import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
-import static net.kyori.adventure.text.Component.text;
+import static io.papermc.paper.command.brigadier.Commands.argument;
+import static io.papermc.paper.command.brigadier.Commands.literal;
 
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "UnstableApiUsage"})
 public class TestSubCommand extends SubCommand {
     public TestSubCommand(StreamLabs pluginInstance) {
         super(pluginInstance);
     }
 
     @Override
-    public String getName() {
-        return "test";
+    public LiteralCommandNode<CommandSourceStack> buildCommand() {
+        return literal("test")
+                .then(argument("event", EventArgumentType.event())
+                        .then(placeholderArg(false))
+                        .then(literal("bypassratelimiters")
+                                .executes(ctx -> executeTest(ctx, true, Set.of()))
+                                .then(placeholderArg(true)))
+                        .executes(ctx -> executeTest(ctx, false, Set.of()))).build();
     }
 
-    @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] args) {
-        if (args.length < 2) {
-            Translations.sendPrefixedResponse("streamlabs.commands.test.error_no_event_type", ColorScheme.INVALID, sender);
-            return true;
-        }
-
-        StreamlabsEvent event = StreamlabsEvent.findEventClasses().stream()
-                .filter(e -> e.getId().equals(args[1]))
-                .findFirst()
-                .orElse(null);
-        if (event == null) {
-            Translations.sendPrefixedResponse("streamlabs.commands.test.error_unknown_event_type", ColorScheme.INVALID, sender, text(args[1]));
-            return true;
-        }
-
-        JsonObject object = new JsonObject();
-        String user = "user%s".formatted(new Random().nextInt(10, 9999999));
-        event.getPlaceholders().removeIf(pl -> !pl.name().startsWith("_"));
-        event.addPlaceholder("user", o -> user);
-        for (int i = 1; i < args.length; i++) {
-            String arg = args[i];
-            if (arg.equals("bypassratelimiters")) {
-                event.bypassRateLimiters();
-                continue;
-            }
-            if (!arg.contains("=")) continue;
-
-            String[] data = arg.split("=");
-            if (data[0].length() <= 1) continue;
-
-            String value2 = data.length > 1 ? data[1] : "";
-            event.addPlaceholder(data[0], o -> value2);
-            try {
-                double value = Double.parseDouble(value2);
-                if (event instanceof YoutubeSuperchatEvent)
-                    value = value * 1000000;
-
-                object.addProperty(data[0], value);
-            } catch (NumberFormatException e) {
-                object.addProperty(data[0], value2);
-            }
-        }
-
-        if (!getPlugin().getExecutor().checkAndExecute(event, object) && getPlugin().showStatusMessages())
-            Translations.sendPrefixedResponse("streamlabs.error.action_failure", ColorScheme.ERROR, sender);
-        return true;
+    private CommandNode<CommandSourceStack> placeholderArg(boolean bypassRateLimiters) {
+        //noinspection unchecked
+        return argument("placeholders", new PlaceholderArgumentType("placeholders"))
+                .executes(ctx -> executeTest(ctx, bypassRateLimiters, ctx.getArgument("placeholders", Set.class)))
+                .build();
     }
 
-    @Override
-    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] args) {
-        if (args.length == 2)
-            return StreamLabs.getCachedEventObjects()
-                    .stream().map(StreamlabsEvent::getId)
-                    .filter(id -> id.contains(args[1]))
-                    .toList();
+    private int executeTest(CommandContext<CommandSourceStack> ctx, boolean bypassRateLimiters, Set<Pair<String, String>> placeholders) {
+        return exceptionHandler(ctx, sender -> {
+            StreamlabsEvent event = EventArgumentType.getEvent(ctx, "event");
+            JsonObject object = new JsonObject();
+            String user = "user%s".formatted(new Random().nextInt(10, 9999999));
+            event.getPlaceholders().removeIf(pl -> !pl.name().startsWith("_"));
+            event.addPlaceholder("user", o -> user);
+            for (Pair<String, String> placeholder : placeholders) {
+                event.addPlaceholder(placeholder.getKey(), o -> placeholder.getValue());
+                try {
+                    double value = Double.parseDouble(placeholder.getValue());
+                    if (event instanceof YoutubeSuperchatEvent)
+                        value = value * 1000000;
 
-        StreamlabsEvent event = StreamLabs.getCachedEventObjects()
-                .stream().filter(e -> e.getId().equals(args[1]))
-                .findFirst().orElse(null);
-        if (event == null) return List.of();
-        if (!args[args.length - 1].contains("=")) {
-            ArrayList<String> placeholders = new ArrayList<>(event.getPlaceholders()
+                    object.addProperty(placeholder.getKey(), value);
+                } catch (NumberFormatException e) {
+                    object.addProperty(placeholder.getKey(), placeholder.getValue());
+                }
+            }
+
+            if (bypassRateLimiters) event.bypassRateLimiters();
+            if (!getPlugin().getExecutor().checkAndExecute(event, object) && getPlugin().showStatusMessages())
+                Translations.sendPrefixedResponse("streamlabs.error.action_failure", ColorScheme.ERROR, sender);
+        });
+    }
+
+    private record PlaceholderArgumentType(
+            String ownName) implements CustomArgumentType<Set<Pair<String, String>>, String> {
+        @Override
+        public @NotNull Set<Pair<String, String>> parse(@NotNull StringReader reader) {
+            String data = reader.getRemaining();
+            reader.setCursor(reader.getTotalLength());
+            String[] elements = data.split(" ");
+            return Arrays.stream(elements)
+                    .map(PlaceholderArgumentType::parseOne)
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toSet());
+        }
+
+        @Nullable
+        private static Pair<String, String> parseOne(String element) {
+            String[] data = element.split("=");
+            if (data.length < 2)
+                return null;
+
+            return Pair.of(data[0], data[1]);
+        }
+
+        @Override
+        public @NotNull ArgumentType<String> getNativeType() {
+            return StringArgumentType.greedyString();
+        }
+
+        @Override
+        public <S> @NotNull CompletableFuture<Suggestions> listSuggestions(CommandContext<S> ctx, @NotNull SuggestionsBuilder builder) {
+            ParsedCommandNode<?> lastNode = ctx.getChild().getNodes().getLast();
+            String input = lastNode.getNode().getName().equals(ownName) ? lastNode.getRange().get(ctx.getInput()) : "";
+            int elementStart =  input.lastIndexOf(' ') + 1;
+            String element = input.substring(elementStart);
+            if (element.contains("=")) return builder.buildFuture();
+
+            StreamlabsEvent event = EventArgumentType.getEvent(ctx.getChild(), "event");
+            event.getPlaceholders()
                     .stream().map(ActionPlaceholder::name)
-                    .filter(name -> !name.startsWith("_") || args[args.length - 1].startsWith("_"))
-                    .toList());
-            if (!Arrays.stream(args).toList().contains("bypassratelimiters"))
-                placeholders.add("bypassratelimiters");
-            return placeholders;
+                    .filter(name -> !name.startsWith("_") || element.startsWith("_"))
+                    .map(name -> input.substring(0, elementStart) + name + "=")
+                    .forEach(builder::suggest);
+            return builder.buildFuture();
         }
-
-        return null;
     }
 }
--- src/main/java/me/Domplanto/streamLabs/command/argument/EventArgumentType.java
+++ src/main/java/me/Domplanto/streamLabs/command/argument/EventArgumentType.java
@@ -7,36 +7,38 @@ import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
-import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.argument.CustomArgumentType;
 import me.Domplanto.streamLabs.StreamLabs;
 import me.Domplanto.streamLabs.command.exception.ComponentCommandExceptionType;
 import me.Domplanto.streamLabs.events.StreamlabsEvent;
+import me.Domplanto.streamLabs.util.ReflectUtil;
 import me.Domplanto.streamLabs.util.components.ColorScheme;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 @SuppressWarnings("UnstableApiUsage")
 public class EventArgumentType implements CustomArgumentType<StreamlabsEvent, String> {
+    private static final List<Class<StreamlabsEvent>> EVENT_CLASSES = ReflectUtil.loadClasses(StreamlabsEvent.class);
     private static final ComponentCommandExceptionType EVENT_NOT_FOUND = new ComponentCommandExceptionType("streamlabs.commands.error.unknown_event_type", ColorScheme.INVALID);
 
     public static EventArgumentType event() {
         return new EventArgumentType();
     }
 
-    public static StreamlabsEvent getEvent(CommandContext<CommandSourceStack> context, String name) {
+    public static StreamlabsEvent getEvent(CommandContext<?> context, String name) {
         return context.getArgument(name, StreamlabsEvent.class);
     }
 
     @Override
     public @NotNull StreamlabsEvent parse(StringReader stringReader) throws CommandSyntaxException {
-        String id = stringReader.getString();
-        //noinspection unchecked
-        Optional<StreamlabsEvent> event = ((Set<StreamlabsEvent>) StreamLabs.getCachedEventObjects())
-                .stream().filter(e -> e.getId().equals(id))
+        String id = stringReader.readString();
+        Optional<StreamlabsEvent> event = EVENT_CLASSES.stream()
+                .map(cls -> ReflectUtil.instantiate(cls, StreamlabsEvent.class))
+                .filter(Objects::nonNull).filter(e -> e.getId().equals(id))
                 .findAny();
         if (event.isEmpty())
             throw EVENT_NOT_FOUND.create();
