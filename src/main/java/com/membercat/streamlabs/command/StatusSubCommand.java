b7f629b897c4b896701066e0e1f533cf275aa07c
codingcat2468 <lw-app-produktion@gmx.net>
2026-05-30 15:49:52 +0200

    Multi-account refactor: status sub-command

--- src/main/java/com/membercat/streamlabs/command/StatusSubCommand.java
+++ src/main/java/com/membercat/streamlabs/command/StatusSubCommand.java
@@ -1,15 +1,21 @@
 package com.membercat.streamlabs.command;
 
+import com.membercat.streamlabs.StreamlabsIntegration;
+import com.membercat.streamlabs.config.PluginConfig;
+import com.membercat.streamlabs.socket.StreamlabsSocketClient;
+import com.membercat.streamlabs.util.components.ColorScheme;
+import com.membercat.streamlabs.util.components.Translations;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
-import com.membercat.streamlabs.StreamlabsIntegration;
-import com.membercat.streamlabs.util.components.ColorScheme;
-import com.membercat.streamlabs.util.components.Translations;
 import net.kyori.adventure.text.Component;
 
-import static net.kyori.adventure.text.Component.text;
-import static net.kyori.adventure.text.Component.translatable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+import static net.kyori.adventure.text.Component.*;
 
 @SuppressWarnings({"unused"})
 public class StatusSubCommand extends SubCommand {
@@ -30,11 +36,19 @@ public class StatusSubCommand extends SubCommand {
     public LiteralCommandNode<CommandSourceStack> buildCommand() {
         return Commands.literal("status")
                 .executes(ctx -> exceptionHandler(ctx, sender -> {
+                    List<PluginConfig.StreamlabsAccount> accounts = new ArrayList<>(getPlugin().pluginConfig().getAccounts());
+                    List<? extends Component> states = accounts.stream().map(a -> {
+                        StreamlabsSocketClient client = getPlugin().getSocketClient(a);
+                        if (client == null) return null;
+                        Component state = empty().append(Collections.nCopies(10 , space()));
+                        state = state.append(translatable("streamlabs.commands.status.format", text(a.id, ColorScheme.DONE), client.isOpen() ? CONNECTED : DISCONNECTED));
+                        return !accounts.getLast().equals(a) ? state.appendNewline() : state;
+                    }).filter(Objects::nonNull).toList();
                     Component status = translatable()
-                            .key("streamlabs.commands.status.prefix")
+                            .key("streamlabs.commands.status.title")
                             .color(ColorScheme.STREAMLABS)
-                            .append(text(" "))
-                            .append(getPlugin().getSocketClient().isOpen() ? CONNECTED : DISCONNECTED)
+                            .append(newline())
+                            .append(states)
                             .build();
 
                     sender.sendMessage(Translations.withPrefix(status, true));
